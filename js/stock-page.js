ff60ad92ce6b990241f3012a722cc41bde1a9444
Bosun Olanrewaju <bosunolanrewaju@gmail.com>
2014-09-27 15:39:03 +0100

    data fetched for both ASI and individual stock from the stockpageAPI

--- js/stock-page.js
+++ js/stock-page.js
@@ -1,16 +1,46 @@
 (function(){
-	var stockModule = angular.module("stock-page-module", []);
+	var stockModule = angular.module("stock-page-module", ["highcharts-ng"]);
 	var base = 'http://api.nse.com.ng/api';
 
-	stockModule.factory("StockPageAPI", function(){
-		//id = null
-		return {
-			symbol: 0,
-			isSelected: function(symbol){
-				this.symbol = symbol;
-			},
-		};
-	});
+	stockModule.factory("StockPageAPI", ["$http", function($http){
+			//id = null
+			return {
+				symbol: 0,
+				symbolData: null,
+				isSelected: function(symbol){
+					this.symbol = symbol;
+				},
+				fetchData: function(symbol){
+						if(symbol === 0){
+							$http.get(base + "/chartdata/ASI").success(function(data){
+								this.stockData = data.IndiciesData;
+							});
+						} else {
+							console.log(this.symbolData);
+							$http.get(base + "/stockchartdata/" + this.symbolData).success(function(data){
+								this.stockData = data;
+							});
+						}
+					}
+			}
+		}]);
+
+	stockModule.factory('StockDataAPI', ['$http', "StockPageAPI", function($http, StockPageAPI){
+		
+			// this.api = StockPageAPI;
+			// this.stockData = [];
+			// if(this.api.symbol === 0){
+			// 	$http.get(base + "/chartdata/ASI").success(function(data){
+			// 		this.stockData: data.IndiciesData;
+			// 	});
+			// } else {
+			// 	$http.get(base + "/stockchartdata/").success(function(data){
+			// 		// return {this.stockData: data.IndiciesData}
+			// 	});
+			// }
+			// return this.stockData;
+
+	}])
 
 	stockModule.directive('stockPage', function(){
 		return {
@@ -26,7 +56,6 @@
 
 				$scope.stockInfo = [];
 				var isSelected = function(symbol){
-					console.log(symbol);
 					if(symbol === 0 || symbol === undefined){
 						$scope.show = false;
 					} else {
@@ -41,9 +70,58 @@
 
 					$http.get(base + "/issuers/companydirectory", config).success(function(data){
 						$scope.stockInfo = data[0];
+						if($scope.stockInfo){
+							$scope.api.symbolData = $scope.stockInfo.InternationSecIN
+							console.log($scope.api.symbolData);
+							$scope.api.fetchData($scope.api.symbol);
+							// fetchData($scope.api.symbol);
+						}
 					});
-				};
+				};			
 			}
 		}
 	});
+
+	stockModule.controller('ChartController', ['$scope', "$http", function($scope, $http){
+		$scope.$watch(function(){
+			return $scope.api.stockData;
+		}, function(){
+			console.log($scope.api.stockData);
+		})
+		
+		var chart = function(symbol, selected, data){
+			$scope.stockChart = {
+					options: {
+						chart: {
+							type: 'line',
+							backgroundColor: 'rgb(240,240,240)'
+						}
+					},
+
+		            rangeSelector : {
+		                selected : selected
+		            },
+
+		            title : {
+		                text : symbol === 0 ? "All Share Index" : symbol
+		            },
+
+		            series : [{
+		                name : symbol === 0 ? "ASI" : symbol,
+		                data : data
+		            }],
+
+		            xAxis: {
+			            type: 'datetime',
+			            title: {text: 'Years'}
+			        },
+
+		            yAxis: {
+			            title: {text: 'Market Capitalization'}
+			        }
+			}
+		}
+
+		chart($scope.api.symbol, 5, $scope.api.stockData);
+	}]);
 })();
