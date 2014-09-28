5a67e06cad11e42238bfaad04224e12fcaad5d8b
Bosun Olanrewaju <bosunolanrewaju@gmail.com>
2014-09-28 02:08:43 +0100

    All share index chart rendered and its link added to the symbol list

--- js/stock-page.js
+++ js/stock-page.js
@@ -9,49 +9,98 @@
 				symbolData: null,
 				isSelected: function(symbol){
 					this.symbol = symbol;
-				},
-				fetchData: function(symbol){
-						if(symbol === 0){
-							$http.get(base + "/chartdata/ASI").success(function(data){
-								this.stockData = data.IndiciesData;
-							});
-						} else {
-							console.log(this.symbolData);
-							$http.get(base + "/stockchartdata/" + this.symbolData).success(function(data){
-								this.stockData = data;
-							});
-						}
-					}
-			}
+				}
+			}				
 		}]);
 
-	stockModule.factory('StockDataAPI', ['$http', "StockPageAPI", function($http, StockPageAPI){
-		
-			// this.api = StockPageAPI;
-			// this.stockData = [];
-			// if(this.api.symbol === 0){
-			// 	$http.get(base + "/chartdata/ASI").success(function(data){
-			// 		this.stockData: data.IndiciesData;
-			// 	});
-			// } else {
-			// 	$http.get(base + "/stockchartdata/").success(function(data){
-			// 		// return {this.stockData: data.IndiciesData}
-			// 	});
-			// }
-			// return this.stockData;
+	stockModule.service('StockDataService', ["$http", function($http){
+				var newSymbol = 0;
+				var stockData = []
+	
+				return {
+					sharedObject: {
+						getSymbol: function(){
+							return newSymbol;
+						},
+						setSymbol: function(symbol){
+							newSymbol = symbol;
+						},
+						set: function(symbol){
+							newSymbol = symbol;
+							// this.fetchData();
+						},
+						fetchData: function(){
+							if(newSymbol === 0){
+								var promise = $http.get(base + "/chartdata/ASI").success(function(data){
+									// newSymbol++;
+									console.log(newSymbol);
+									return  data.IndiciesData;
+								});
+							} else {
+								var promise = $http.get(base + "/stockchartdata/" + newSymbol).success(function(data){
+									return data;
+								});
+							}
+							return promise;
+						},
+						getStockData: function(){
+							return stockData;
+						},
 
-	}])
+						drawChart: function(container, symbol, selected, data){
+							console.log("drawn");
+							return {
+								options: {
+									chart: {
+										renderTo: container,
+										type: 'line',
+										height: 400,
+										backgroundColor: 'rgb(240,240,240)'
+									}
+								},
+
+								height: 400,
+
+					            rangeSelector : {
+					                selected : selected
+					            },
+
+					            title : {
+					                text : symbol === 0 ? "All Share Index" : symbol
+					            },
+
+					            series : [{
+					                name : symbol === 0 ? "ASI" : symbol,
+					                data : data
+					            }],
+
+					            xAxis: {
+						            type: 'datetime',
+						            title: {text: 'Years'}
+						        },
+
+					            yAxis: {
+						            title: {text: 'Market Capitalization'}
+						        }
+							}
+						}
+					}
+				}
+		
+			}])
 
 	stockModule.directive('stockPage', function(){
 		return {
 			restrict: 'E',
 			templateUrl: 'stock-page.html',
-			controller: function(StockPageAPI, $scope, $http){
+			controller: function(StockPageAPI, $scope, $http, StockDataService){
 				$scope.api = StockPageAPI;
+				$scope.dService = StockDataService.sharedObject
 				$scope.$watch(function(){
 					return $scope.api.symbol
 				}, function(){
 					isSelected($scope.api.symbol);
+					// $scope.dService.setSymbol($scope.api.symbol);
 				});
 
 				$scope.stockInfo = [];
@@ -72,8 +121,7 @@
 						$scope.stockInfo = data[0];
 						if($scope.stockInfo){
 							$scope.api.symbolData = $scope.stockInfo.InternationSecIN
-							console.log($scope.api.symbolData);
-							$scope.api.fetchData($scope.api.symbol);
+							$scope.dService.set($scope.api.symbolData);
 							// fetchData($scope.api.symbol);
 						}
 					});
@@ -82,46 +130,20 @@
 		}
 	});
 
-	stockModule.controller('ChartController', ['$scope', "$http", function($scope, $http){
-		$scope.$watch(function(){
-			return $scope.api.stockData;
-		}, function(){
-			console.log($scope.api.stockData);
-		})
-		
-		var chart = function(symbol, selected, data){
-			$scope.stockChart = {
-					options: {
-						chart: {
-							type: 'line',
-							backgroundColor: 'rgb(240,240,240)'
-						}
-					},
-
-		            rangeSelector : {
-		                selected : selected
-		            },
-
-		            title : {
-		                text : symbol === 0 ? "All Share Index" : symbol
-		            },
-
-		            series : [{
-		                name : symbol === 0 ? "ASI" : symbol,
-		                data : data
-		            }],
-
-		            xAxis: {
-			            type: 'datetime',
-			            title: {text: 'Years'}
-			        },
-
-		            yAxis: {
-			            title: {text: 'Market Capitalization'}
-			        }
-			}
-		}
-
-		chart($scope.api.symbol, 5, $scope.api.stockData);
+	stockModule.controller('ChartController', ['$scope', "StockDataService", function($scope, StockDataService){
+			$scope.$watch(function(){
+				return StockDataService.sharedObject.getSymbol();
+			}, function(){
+				StockDataService.sharedObject.fetchData().success(function(data){
+					$scope.stockChart = StockDataService.sharedObject.drawChart("symbol_chart", $scope.api.symbol, 1, data);
+				})
+			});
 	}]);
+
+	stockModule.controller('asiController', ['StockDataService', '$scope', function(StockDataService, $scope){
+		StockDataService.sharedObject.fetchData().success(function(data){
+			console.log(data.IndiciesData);
+			$scope.asiChart = StockDataService.sharedObject.drawChart("asi_chart", StockDataService.sharedObject.getSymbol(), 5, data.IndiciesData);
+		});		
+	}])
 })();
